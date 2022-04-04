58711d94eb54b1c59dcdfa3101c8c0fb27c7f642
Vicky Banks <victoria.banks052@gmail.com>
2022-04-04 18:08:21 +0100

    Created graph of age & gender breakdown

--- R/basic_news_stats.R
+++ R/basic_news_stats.R
@@ -6,7 +6,13 @@ library(lubridate)
 library(httr)
 library(ggplot2)
 library(scales)
+library(RColorBrewer)
+library(wesanderson)
 theme_set(theme_classic())
+
+##### functions ######
+### round up/down to nearest value for the axes limits
+round_any <- function(x, accuracy, func){func(x/ accuracy) * accuracy}
 
 ######### Get Redshift creds (local R) #########
 driver <-JDBC("com.amazon.redshift.jdbc41.Driver","~/.redshiftTools/redshift-driver.jar",identifier.quote = "`")
@@ -28,10 +34,58 @@ dbGetQuery(conn,"select distinct brand_title, series_title  from prez.scv_vmb li
 
 #### Basic Demographics
 data<-dbGetQuery(conn, "SELECT * FROM central_insights_sandbox.vb_news_basics;")
+data$users<-as.numeric(data$users)
+data$app_type<-factor(data$app_type,levels = c('mobile-chrysalis','app','web') )
 data %>% head()
 
 
 
+### age_gender
+plot_data<-data %>%
+  group_by(app_type,age_range, gender) %>%
+  summarise(users=sum(users)) %>% 
+  filter(age_range !='Unknown')
+plot_data
+
+perc_labs<-plot_data %>%ungroup() %>%group_by(app_type,age_range)%>%mutate(perc = round(100*users/sum(users),0))
+perc_labs
+age_perc<-plot_data %>%ungroup() %>%group_by(app_type,age_range)%>%summarise(users = sum(users)) %>%mutate(perc = round(100*users/sum(users),0))
+age_perc
+
+age_perc_labs <- perc_labs %>%
+  select(app_type, age_range, gender) %>%
+  left_join(age_perc, by = c('age_range', 'app_type')) %>%
+  replace(is.na(.), 0) %>%
+  left_join(
+    plot_data %>%
+      summarise(total = sum(users) / 1000000) %>%
+      group_by(app_type) %>%
+      summarise(max_value = max(total))
+  )
+
+
+
+
+ggplot(data = plot_data ,
+       aes(x = age_range,y = users/1000000, group = gender, fill = gender)) +
+  geom_col(inherit.aes = TRUE,
+           position = "stack",
+           show.legend = TRUE)+
+  #scale_y_continuous(limits = c(0, max_value$total*1.1), breaks = seq(0, max_value$total*1.1, by = 1))+
+  xlab("Age Range") +
+  ylab("Users (millions)") +
+  labs(title = paste0('Age & gender distribution of News users \n(2022-01-15 to 2022-03-31)'))+
+  geom_text(aes(label=paste0(perc_labs$perc ,"%")),
+            position=position_stack(vjust = 0.5),
+            colour="black")+
+  geom_label(data = age_perc_labs,
+             aes(label=paste0(age_perc_labs$perc ,"%")),
+             y = age_perc_labs$max_value,
+             colour="black",
+             fill = "white")+
+  scale_fill_manual(name = "Gender",values=wes_palette(n=3, name="GrandBudapest1"))+
+  theme(legend.position = "bottom")+
+  facet_wrap(~app_type, scales = "free_y")
 
 
 
