4d1fc3a7ba29cd743ed297dcc78cab0e053c903e
Vicky Banks <victoria.banks052@gmail.com>
2022-05-13 16:08:14 +0100

    Grouped topics that are regions and removed least used topics to reduce the feature set

--- sql/get_topics_visited.sql
+++ sql/get_topics_visited.sql
@@ -21,15 +21,83 @@ with get_pages as (
       AND page_section NOT ILIKE 'name=%'
 
 )
-SELECT audience_id, page_section, count(*) as topic_count
-FROM get_pages
-GROUP BY 1,2
-ORDER BY 1,3
-;
+SELECT audience_id,
+       CASE
+           WHEN page_section in ('beds_bucks_and_herts',
+                                 'berkshire',
+                                 'birmingham_and_black_country',
+                                 'bradford_and_west_yorkshire',
+                                 'bristol',
+                                 'cambridgeshire',
+                                 'cornwall',
+                                 'coventry_and_warwickshire',
+                                 'cumbria',
+                                 'derbyshire',
+                                 'devon',
+                                 'dorset',
+                                 'essex',
+                                 'gloucestershire',
+                                 'hampshire',
+                                 'hereford_and_worcester',
+                                 'humberside',
+                                 'kent',
+                                 'lancashire',
+                                 'leeds_and_west_yorkshire',
+                                 'leicester',
+                                 'lincolnshire',
+                                 'manchester',
+                                 'merseyside',
+                                 'norfolk',
+                                 'northamptonshire',
+                                 'nottingham',
+                                 'oxford',
+                                 'shropshire',
+                                 'somerset',
+                                 'south_yorkshire',
+                                 'stoke_and_staffordshire',
+                                 'suffolk',
+                                 'surrey',
+                                 'sussex',
+                                 'tayside_and_central',
+                                 'tees',
+                                 'tyne_and_wear',
+                                 'wiltshire',
+                                 'york_and_north_yorkshire') THEN 'region_england'
+           WHEN page_section IN ('guernsey',
+                                 'isle_of_man',
+                                 'jersey') THEN 'region_islands'
+           WHEN page_section IN ('foyle_and_west') THEN 'region_northern_ireland'
+           WHEN page_section IN ('edinburgh_east_and_fife',
+                                 'glasgow_and_west',
+                                 'highlands_and_islands',
+                                 'north_east_orkney_and_shetland',
+                                 'south_scotland') THEN 'region_scotland'
+           WHEN page_section IN ('mid_wales',
+                                 'north_east_wales',
+                                 'north_west_wales',
+                                 'south_east_wales',
+                                 'south_west_wales') THEN 'region_wales'
+           ELSE page_section END as page_section,
 
+       count(*)                  as topic_count
+FROM get_pages
+GROUP BY 1, 2
+ORDER BY 1, 3
+;
+--remove any topic with less than 100 visits
+CREATE TEMP TABLE vb_section_usage as
+SELECT page_section, count(distinct audience_id) as users, sum(topic_count) as count  FROM vb_page_topics GROUP BY 1
+HAVING count <1000
+ORDER BY 3 desc;
+
+DELETE FROM vb_page_topics WHERE page_section IN (SELECT page_section FROM vb_section_usage );
+
+--checks
 SELECT * FROM vb_page_topics ORDER BY audience_id, topic_count DESC LIMIT 10;
 SELECT count(distinct audience_id) FROM vb_page_topics  LIMIT 10; --9,275,293
 SELECT count(*) FROM vb_page_topics  LIMIT 10; --82,048,718
+
+
 
 DROP TABLE IF EXISTS vb_page_topics_perc;
 CREATE TABLE vb_page_topics_perc as
@@ -43,7 +111,7 @@ ORDER BY a.audience_id
 
 --checks
 SELECT DISTINCT 'dummy'::varchar as audience_id, page_section, 0::double precision as topic_perc FROM central_insights_sandbox.vb_page_topics_perc ORDER BY 2;
-SELECT count(distinct page_section) FROM vb_page_topics_perc    LIMIT 10;--147
+SELECT count(distinct page_section) FROM vb_page_topics_perc    LIMIT 10;--67
 SELECT * FROM central_insights_sandbox.vb_page_topics_perc    LIMIT 10;
 SELECT count(*) FROM vb_page_topics_perc    LIMIT 10;--82,048,718
 
