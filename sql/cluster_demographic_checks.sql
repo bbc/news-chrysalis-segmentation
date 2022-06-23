6e881c7f1a1436d510d3051391724f2ee8d44951
Vicky Banks <victoria.banks052@gmail.com>
2022-06-23 17:53:14 +0100

    Code to look at demographics of the clusters

---
+++ sql/cluster_demographic_checks.sql
@@ -0,0 +1,41 @@
+set search_path TO 'central_insights_sandbox';
+
+DROP TABLE IF EXISTS vb_cluster_demo;
+CREATE TABLE vb_cluster_demo as
+with get_data as (
+    SELECT a.*,
+           CASE
+               WHEN b.age_range in ('16-19', '20-24') THEN '16-24'
+               WHEN b.age_range in ('25-29', '30-34') THEN '25-34'
+               WHEN b.age_range in ('35-39', '40-44') THEN '35-44'
+               WHEN b.age_range in ('45-49', '50-54') THEN '45-54'
+               WHEN b.age_range ISNULL THEN 'unknown'
+               ELSE '55+' END                                  as age_range,
+           CASE
+               WHEN gender = 'male' THEN 'male'
+               WHEN gender = 'female' THEN 'female'
+               ELSE 'unknown' END                              as gender
+            ,
+           acorn_category || '-' || acorn_category_description as acorn_category,
+           nation
+    FROM vb_chrys_feat_test a
+             LEFT JOIN prez.profile_extension b on a.audience_id = b.bbc_hid3
+)
+SELECT cluster, age_range, gender, acorn_category, nation, count(audience_id) as users
+FROM get_data
+GROUP BY 1, 2, 3, 4, 5
+;
+
+SELECT cluster, age_range, count(audience_id) as users
+FROM vb_cluster_demo
+GROUP BY 1, 2
+ORDER BY 1, 2;
+
+SELECT * FROM vb_cluster_demo ORDER BY cluster, nation, age_range, gender, acorn_category;
+SELECT distinct age_range FROM prez.profile_extension
+
+--- Who are the people in the boring cluster?
+SELECT *
+FROM vb_chrys_feat_test
+WHERE cluster = 0
+LIMIT 10;
